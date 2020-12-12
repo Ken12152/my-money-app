6cb0de2ae53c41d41605ea90accb0b15464f4c3a
Felipe Kenzo <Kenzo12152@gmail.com>
2020-12-12 19:17:26 -0300

    fix run and cors

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Contro-Allow-Origin', '*')
-    res.header('Access-Contro-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Access-Contro-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept') 
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept') 
     next()
 }
---
+++ frontend/src/dashboard/dashboardReducer.jsx
@@ -0,0 +1,10 @@
+const DEFAULT_STATE = { summary: { credit: 0, debt: 0 } }
+
+export default function(state = DEFAULT_STATE, action) {
+    switch(action.type) {
+        case 'BILLING_SUMMARY_FETCHED':
+            return { ...state, summary: action.payload.data }
+        default:
+            return state
+    }
+}
