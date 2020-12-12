d998904db7c383edfc1c2ebe43eed05588c409db
Felipe Kenzo <Kenzo12152@gmail.com>
2020-12-11 22:07:21 -0300

    end backend

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,9 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('get', (req, res, next) => {
     BillingCycle.find({}, (error, docs) => {
@@ -10,13 +12,29 @@ BillingCycle.route('get', (req, res, next) => {
         } else {
             res.status(500).json({ errors: [error] })
         }
-    })
+    }).skip(req.query.skip).limit(req.query.limit)
 })
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
         if(!error) {
             res.json({ value })
+        } else {
+            res.status(500).json({ errors: [error] })
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{
+        $project: { debt: { $sum: "$debts.value" }, credit: { $sum: "$credits.value"}}
+    }, {
+        $group: { _id: null, debt: { $sum: "$debt" }, credit: { $sum: "$credit" }}
+    }, {
+        $project: { _id: 0,  debt: 1, credit: 1 }
+    }], (error, result) => {
+        if(!error) {
+            res.json(result[0] || { debt: 0, credit: 0 })
         } else {
             res.status(500).json({ errors: [error] })
         }
---
+++ backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+    res.header('Access-Contro-Allow-Origin', '*')
+    res.header('Access-Contro-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Contro-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept') 
+    next()
+}
