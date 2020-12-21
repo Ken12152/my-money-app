88e9ba98ccd7aedb059b13e060d79c226615860b
Felipe Kenzo <Kenzo12152@gmail.com>
2020-12-20 22:41:57 -0300

    _select

--- frontend/src/billingCycle/summary.jsx
+++ frontend/src/billingCycle/summary.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ValueBox from '../common/widget/valueBox'
+import Grid from '../common/layout/grid'
+import Row from '../common/layout/row'
 
 export default props => (
-    <div>
-        <ValueBox cols="12 4" color="green" icon="bank"
-            value={`R$ ${props.credits}`} text="Credits" />
-        <ValueBox cols="12 4" color="red" icon="credit-card"
-            value={`R$ ${props.debts}`} text="Debts" />
-        <ValueBox cols="12 4" color="blue" icon="money"
-            value={`R$ ${props.credits - props.debts}`} text="Consolidado" />
-    </div>
+    <Grid>
+        <fieldset>
+            <legend>Summary</legend>
+            <Row>
+                <ValueBox cols="12 4" color="green" icon="bank"
+                    value={`R$ ${props.credits}`} text="Credits" />
+                <ValueBox cols="12 4" color="red" icon="credit-card"
+                    value={`R$ ${props.debts}`} text="Debts" />
+                <ValueBox cols="12 4" color="blue" icon="money"
+                    value={`R$ ${props.credits - props.debts}`} text="Consolidado" />
+            </Row>
+        </fieldset>
+    </Grid>
 )
